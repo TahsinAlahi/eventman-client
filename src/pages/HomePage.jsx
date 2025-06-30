b0dafa4ab8d9eade42390e2845f632e0a7698867
Tahsin Alahi <alahitahsin@gmail.com>
2025-06-30 17:26:32 +0600

    refactor: Homepage ui animation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,65 +1,79 @@
 import { Link } from "react-router";
-import bgImage from "../assets/bg-image.jpg";
 import { useAuth } from "../providers/AuthProvider";
+import Squares from "../ui/Square";
+import BlurIn from "../ui/BlurText";
 
 function HomePage() {
   const { user } = useAuth();
 
   return (
-    <div
-      className="w-screen h-[calc(100vh-56px)] lg:h-[calc(100vh-58px)] bg-slate-900 bg-cover bg-center  text-white"
-      // style={{
-      //   backgroundImage: `url(${bgImage})`,
-      // }}
-    >
-      <main className="max-w-screen-2xl h-full mx-auto flex items-center justify-center ">
-        <div className="max-w-xl mx-auto flex items-center justify-center flex-col text-white p-8 h-min ">
-          <div className="text-center space-y-6">
-            <h1 className="md:text-4xl text-2xl font-bold font-rubik">
-              Welcome to EventMan
-            </h1>
-            <p class="text-lg w-11/12 mx-auto px-3 font-poppins text-center">
-              <strong>
-                Explore exciting events, grow your network, and make every
-                moment count! 🎉📅
-              </strong>
-              <br />
-              From <em>workshops</em> to <em>community drives</em>—find, join,
-              and be part of something bigger.{" "}
-              <span class="inline-block">🌟🙌</span>
-            </p>
-          </div>
+    <>
+      <div className="relative bg-black w-screen h-[calc(100vh-56px)] lg:h-[calc(100vh-58px)] overflow-hidden">
+        <Squares
+          speed={0.5}
+          squareSize={40}
+          direction="diagonal"
+          borderColor="#334155"
+          hoverFillColor="#222"
+        />
 
-          <div className="space-x-3 text-lg mt-8">
-            {user?._id ? (
-              <>
-                <Link
-                  to="/events"
-                  className="font-semibold text-white bg-slate-950 hover:bg-secondary hover:bg-white hover:text-slate-950 px-5 py-2 transition duration-300 "
+        <main className="absolute bg-black/40 inset-0 flex items-center justify-center">
+          <div className="max-w-xl  mx-auto flex items-center justify-center flex-col text-neutral-300 p-8">
+            <div className="text-center flex flex-col justify-center items-center space-y-6">
+              <BlurIn className="md:text-4xl text-center text-2xl font-bold font-rubik">
+                Welcome to EventMan
+              </BlurIn>
+              <div>
+                <BlurIn
+                  duration={1.5}
+                  className="text-lg w-11/12 mx-auto px-3 font-poppins text-center font-semibold"
                 >
-                  Events
-                </Link>
-              </>
-            ) : (
-              <>
-                <Link
-                  to="/login"
-                  className="font-semibold text-white bg-slate-950 hover:bg-secondary hover:bg-white hover:text-slate-950 px-5 py-2 transition duration-300 "
+                  Explore exciting events, grow your network, and make every
+                  moment count! 🎉📅
+                </BlurIn>
+                <br />
+                <BlurIn
+                  className="text-lg w-11/12 mx-auto px-3 font-poppins text-center"
+                  duration={2}
                 >
-                  Login
-                </Link>
-                <Link
-                  to="/signup"
-                  className="font-semibold text-white bg-slate-950 hover:bg-secondary hover:text-slate-950 hover:bg-white px-5 py-2 transition duration-300"
+                  From <em>workshops</em> to <em>community drives</em> — find,
+                  join, and be part of something bigger.{" "}
+                  <span className="inline-block">🌟🙌</span>
+                </BlurIn>
+              </div>
+            </div>
+
+            <div className="space-x-3 text-lg mt-8">
+              {user?._id ? (
+                <BlurIn
+                  className="font-semibold text-neutral-950 bg-neutral-300 hover:bg-neutral-200 hover:text-neutral-950 px-5 py-2 transition duration-300"
+                  duration={2.3}
                 >
-                  Sign Up
-                </Link>
-              </>
-            )}
+                  <Link to="/events">Events</Link>
+                </BlurIn>
+              ) : (
+                <>
+                  <BlurIn className="space-x-5">
+                    <Link
+                      to="/login"
+                      className="font-semibold text-neutral-950 bg-neutral-300 hover:bg-neutral-200 hover:text-neutral-950 px-5 py-2 transition duration-300"
+                    >
+                      Login
+                    </Link>
+                    <Link
+                      to="/signup"
+                      className="font-semibold text-neutral-950 bg-neutral-300 hover:bg-neutral-200 hover:text-neutral-950 px-5 py-2 transition duration-300"
+                    >
+                      Sign Up
+                    </Link>
+                  </BlurIn>
+                </>
+              )}
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </>
   );
 }
 
